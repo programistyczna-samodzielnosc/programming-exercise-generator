194b61e03efdf02fd0226d57aac9ad6261085c29
Cyprian Gepfert <cgepfert@gmail.com>
2019-04-14 17:30:27 +0200

    uwzglednianie przeszlych zadan

--- README.md
+++ README.md
@@ -3,3 +3,8 @@ Program ktory umozliwia generawanie zadanek programistycznych różnych typów
 
 * zadanka z html i css - tworzenie prostych szablonow
 * zadanka sredniozaawansowane z Javascript - tworzenie funkcji z biblioteki underscore
+
+
+## Plan rozwoju
+
+* zapisywanie przeszlych zadan w bazie danych
--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 const underscore = require('underscore')
 
+
 let keys = Object.keys(underscore)
-let exceptions = ['VERSION','_'];
-keys = keys.filter((name)=> !exceptions.includes(name))
+let exceptions = ['VERSION', '_'];
 
-// console.log(keys.length)
-// console.log(keys);
+let pastExercises = [];
 
-//zrobic losowanie liczby od 1-139
+let allExceptions = exceptions.concat(pastExercises)
+
+keys = keys.filter((name) => !allExceptions.includes(name))
 
 function getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max));
 }
 
-let nrZadania = getRandomInt(139)
+let nrZadania = getRandomInt(keys.length)
+
+console.log("Liczba pozostałych zadan: " + keys.length)
 
 console.log("Zaimplementuj funkcje " + keys[nrZadania])
 
