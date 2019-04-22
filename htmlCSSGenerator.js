987c188e5e67c4a114099bf285e110f40db52d73
Cyprian Gepfert <cgepfert@gmail.com>
2019-04-22 20:03:31 +0200

    sprzatanie smieci po starym generatorze zadan

--- htmlCSSGenerator.js
+++ htmlCSSGenerator.js
@@ -1,8 +1,8 @@
 console.log(`Zadanie domowe: ${new Date()}`)
+console.log('Stworz layout strony internetowej: ')
 
 const helpers = require('./helpers')
 
-const maxOfElements = 5 // 4el
 let elementsCount1 = 3
 
 function direction(childrenCount) {
@@ -13,9 +13,6 @@ function direction(childrenCount) {
     }
     return ''
 }
-
-console.log(`Layout zawiera ${elementsCount1} elementy ${direction(elementsCount1)}`)
-
 
 //generujemy drzewko o trudnosci n (np 10)
 
@@ -41,35 +38,10 @@ function createTree(difficulty=4, maxOfBranches=3, indent=0) {
 
 createTree()
 
-
-// for (let i = 1; i <= elementsCount1; i++) {
-//     let elementsCount2 = helpers.getRandomInt(maxOfElements)
-//     printElements(i, elementsCount2, 2)
-
-//     for (let j = 1; j <= elementsCount2; j++) {
-//         let elementsCount3 = helpers.getRandomInt(maxOfElements)
-//         printElements(j, elementsCount3, 4)
-//         //
-//         // for (let k = 1; k <= elementsCount3; k++) {
-//         //     let elementsCount4 = helpers.getRandomInt(maxOfElements)
-//         //     printElements(k, elementsCount4, 6)
-//             //
-//             // for (let l = 1; l <= elementsCount4; l++) {
-//             //     let elementsCount5 = helpers.getRandomInt(maxOfElements)
-//             //     printElements(l, elementsCount5, 6)
-//             // }
-//         // }
-//     }
-// }
-
 function getFinalIndent(indent) {
     let finalIndent = ""
     for (let i = 0; i < indent; i++) {
         finalIndent += "-"
     }
     return finalIndent + ">"
-}
-
-function printElements(parentNumber, childrenCount, indent) {
-    console.log(`${getFinalIndent(indent)}Element ${parentNumber} zawiera ${childrenCount} el. ${direction(childrenCount)}`)
 }
